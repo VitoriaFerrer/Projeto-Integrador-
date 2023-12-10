a1222dac68d1f07c90d2fbdf0684b7845f181a59
Vitória Ferreira <123346707+VitoriaFerrer@users.noreply.github.com>
2023-12-10 19:15:53 -0300

    Add files via upload
    Documentos do site feito, porém ainda não está responsivo

---
+++ scripts.js
@@ -0,0 +1,7 @@
+document.getElementById("name="" method="" action=""").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    // Simulação de envio de formulário
+    alert("Mensagem enviada com sucesso!");
+    document.getElementById("name="" method="" action=""").reset();
+});
